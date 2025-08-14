41772fb6c18df8363d149d89a64a384134ba31b1
anotherparsa <anotherparsa@gmail.com>
2025-08-14 18:04:13 +0330

    tidy up the structure

--- djangoproject/todoapp/todoapp/urls.py
+++ djangoproject/todoapp/todoapp/urls.py
@@ -15,13 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-def home_page(request):
-    return HttpResponse("This is the home page")
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home_page),
+    path('', include('todo.urls')),
 ]
