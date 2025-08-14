6c0f0e3245570a920b232ebc3dca4cfb3f69fae4
anotherparsa <anotherparsa@gmail.com>
2025-08-14 20:04:25 +0330

    for loop tag in template

--- djangoproject/todoapp/todo/views.py
+++ djangoproject/todoapp/todo/views.py
@@ -6,4 +6,24 @@ def home_page(request):
     return render(request, 'todo/homepage.html', contextdic)
 
 def about_us_page(request):
-    return render(request, 'todo/aboutus.html')
+    clients_list = [
+        {
+        "name" : "Testname1",
+        "family" : "testfamily1",
+        "age" : "2221", 
+        },
+        {
+        "name" : "Testname2",
+        "family" : "testfamily2",
+        "age" : "2222", 
+        },
+        {
+        "name" : "Testname3",
+        "family" : "testfamily3",
+        "age" : "2223", 
+        },
+    ]
+    contextdic = {
+        'clientlist' : clients_list
+    }
+    return render(request, 'todo/aboutus.html', contextdic)
