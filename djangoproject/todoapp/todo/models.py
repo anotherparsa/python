a3120f33838437728cdc2d3bab365d6335d32fa0
anotherparsa <anotherparsa@gmail.com>
2025-08-14 21:24:57 +0330

    defining models

--- djangoproject/todoapp/todo/models.py
+++ djangoproject/todoapp/todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField()
+    password = models.CharField()
+
+
+class Task(models.Model):
+    title = models.CharField()
+    task = models.CharField()
+    created = models.DateTimeField(auto_now_add=True)
