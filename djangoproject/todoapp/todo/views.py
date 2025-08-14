51a9b49ee19bff0f59087ccffed3e5c1ceeeb03e
anotherparsa <anotherparsa@gmail.com>
2025-08-14 19:43:46 +0330

    passing data to the template

--- djangoproject/todoapp/todo/views.py
+++ djangoproject/todoapp/todo/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home_page(request):
-    return render(request, 'todo/homepage.html')
+    contextdic = {"datakey" : "datavalue"}
+    return render(request, 'todo/homepage.html', contextdic)
 
 def about_us_page(request):
     return render(request, 'todo/aboutus.html')
